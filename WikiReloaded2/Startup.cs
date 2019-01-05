b9425ff2dd3f22bb8be29b4598d0fe3e431d2018
Viorel <viorel.popescu@mail.com>
2019-01-05 12:20:45 +0200

    Add project files.

---
+++ WikiReloaded2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WikiReloaded2.Startup))]
+namespace WikiReloaded2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
